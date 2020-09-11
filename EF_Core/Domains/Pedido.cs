98603d18c17e63dcff4bbec5872e95b56f6680ec
Murilo Fernandes Dellize <murilo46dellize@gmail.com>
2020-09-11 00:26:16 -0300

    fff

---
+++ EF_Core/Domains/Pedido.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EF_Core.Domains
+{
+    public class Pedido : BaseDomains
+    {
+        public string Status { get; set; }
+        public DateTime OrderDate { get; set; }
+
+    }
+}
---
+++ EF_Core/Domains/Produto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EF_Core.Domains
+{
+
+    //Define a classe do produto
+    public class Produto : BaseDomains
+    {
+     
+        public string Nome { get; set; }
+        public float Preco { get; set; }
+
+    }
+
+}
